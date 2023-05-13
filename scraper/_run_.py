8d1207c347d179ea22267fec415ed1f433b10dff
Max Ruffo <Max.Ruffo@Student.HTW-Berlin.de>
2023-05-13 14:07:48 +0200

    memory leak gefunden data/index_list dow jones wird nochmal erstellt

---
+++ scraper/_run_.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+folders = ["asia", "europe", "usa"]
+
+for folder in folders:
+    folder_path = os.path.join("scraper", folder)
+    if os.path.isdir(folder_path):
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                if file.endswith(".py"):
+                    file_path = os.path.join(root, file)
+                    print(f"Ausführen der Datei: {file_path}")
+                    subprocess.run(["python3", file_path], check=True)
+                    print(f"Die Datei wurde erfolgreich ausgeführt: {file_path}")
+    else:
+        print(f"Der Ordner {folder_path} existiert nicht.")
