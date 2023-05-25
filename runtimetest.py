f172685a4fa407763f245fd3c05b077f99233985
Max Ruffo <Max.Ruffo@Student.HTW-Berlin.de>
2023-05-25 12:49:45 +0200

    daten

--- runtimetest.py
+++ runtimetest.py
@@ -1,10 +1,10 @@
 import yfinance as yf
 import datetime as datetime2
 
-today = datetime.date.today()
-yesterday = today - datetime.timedelta(days=1)
+today = datetime2.date.today()
+yesterday = today - datetime2.timedelta(days=1)
 
 
-data = yf.download("AMC", period='1d', interval='1m',start=yesterday,end=today, progress=False)
+data = yf.download("BTC-USD", period='1d', interval='1m',start=yesterday,end=today, progress=False)
 
 print(data)
